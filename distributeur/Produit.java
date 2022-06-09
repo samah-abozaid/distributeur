0c895cf9a3adb7e76b65e1a912509dd5c8bb13c3
samah-abozaid <samah.abozaid89@gmail.com>
2022-06-09 23:09:42 +0200

    class pour le s produits

---
+++ distributeur/Produit.java
@@ -0,0 +1,66 @@
+
+package distributeur;
+
+public class Produit {
+    
+    private int id;
+    private String nom;
+    private int prix;
+    private int quantite;
+
+    public Produit() {
+       
+    }
+        
+    public Produit(int id, String nom, int prix, int quantite) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.quantite = quantite;
+    }
+    
+    
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Produit{" + "id=" + id + ", nom=" + nom + ", prix=" + prix + ", quantite=" + quantite + '}';
+    }
+    
+    
+
+
+    
+    
+}
